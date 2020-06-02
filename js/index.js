7f8d27ec10630668706cbe51caa82fcbd17b5d9f
Stephanie Gatt <stephanielynngatt@gmail.com>
2020-06-02 16:33:46 -0400

    added mouseover, wheel, mouseleave

--- js/index.js
+++ js/index.js
@@ -1,1 +1,51 @@
 // Your code goes here
+// * [ ] Using your [index.js file](js/index.js), create 10 [unique event listeners](https://developer.mozilla.org/en-US/docs/Web/Events). using your creativity to make the Fun Bus site more interactive.  Here are some unique events you could try to use: 
+// 	* [ ] `mouseover`
+// 	* [ ] `keydown`
+// 	* [ ] `wheel`
+// 	* [ ] `drag / drop`
+// 	* [ ] `load`
+// 	* [ ] `focus`
+// 	* [ ] `resize`
+// 	* [ ] `scroll`
+// 	* [ ] `select`
+// 	* [ ] `dblclick`
+
+// Using the 10 unique events, find ways to update the DOM in creative ways. For example you could change colors, animate objects, remove objects, etc.
+
+// * [ ] Nest two similar events somewhere in the site and prevent the event propagation properly
+// * [ ] Stop the navigation items from refreshing the page by using `preventDefault()`
+// mouseover
+const mainNav = document.querySelector(".main-navigation");
+mainNav.addEventListener("mouseover", function( event ) {   
+    event.target.style.color = "#17A2B8";
+    setTimeout(function() {
+      event.target.style.color = "";
+    }, 500);
+  }, false);
+
+  //keydown
+const welcomeImg = document.getElementById("welcome-image");
+
+let scale = 1;
+function zoom(event) {
+    event.preventDefault();
+  
+    scale += event.deltaY * -0.01;
+  
+    // Restrict scale
+    scale = Math.min(Math.max(.125, scale), 4);
+  
+    // Apply scale transform
+    event.target.style.transform = `scale(${scale})`;
+    console.log("this is event.target", event.target)
+  };
+
+welcomeImg.addEventListener("wheel", zoom);
+welcomeImg.addEventListener("mouseleave", event => {
+    event.target.style.transform = "scale(1)";
+  });
+
+
+
+
